2b0d31a2757f638d6cff8aa46bb15a9b2a925b9e
unknown <arin0207@gmail.com>
2020-04-14 21:31:34 +0530

    Pipes, Custom Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Practice';
+  text = 'In the interest of public health and to ensure the well-being of our employees and partners Siemens Stiftung is reducing direct person-to-person contact to help stop the spread of the COVID-19 pandemic. We will continue working – mostly online – to advance the foundation’s issues and projects. As a foundation active internationally, these measures apply globally.';
 }

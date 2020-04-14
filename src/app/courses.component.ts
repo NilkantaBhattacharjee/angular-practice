23e9daf14ce58ecd052b726d897785a83ccb2f37
unknown <arin0207@gmail.com>
2020-04-14 17:11:49 +0530

    Interpolation, Property, Attribute, Class, Style, Event binding, Bootstrap added

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -8,6 +8,11 @@ import { CoursesService } from './courses.service';
                <ul>
                     <li *ngFor='let course of getCourseList()'>{{course}}</li>
                </ul>
+               <div (click)="onDivClick();">
+                    <button class="btn btn-primary" [class.Active]="isActive" [disabled]="!isActive"
+                    [style.background-color]="isActive ? 'Red' : 'Blue'"
+                    (click)="onClick($event)">Save</button>
+               </div>
     `
 })
 
@@ -15,6 +20,9 @@ export class CoursesComponent {
     title = 'List of courses';
     anotherTitle = 'Course';
     // courses = ['course1', 'course2', 'course3'];
+
+    isActive = true;
+
     courses: string[];
     service: CoursesService;
     constructor(service: CoursesService) {
@@ -28,5 +36,15 @@ export class CoursesComponent {
 
     getCourseList(): string[] {
         return this.service.getCourseList();
+    }
+
+    onClick($event: any) {
+        $event.stopPropagation();
+        console.log('Button was clicked');
+        console.log($event);
+    }
+
+    onDivClick($event: any) {
+        console.log('Div was clicked');
     }
 }
